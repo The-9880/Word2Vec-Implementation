66493db7bcf77c5ed021622967495f79d8ababd0
Anant Sharma <anant98@hotmail.com>
2018-01-17 21:03:28 -0500

    Primed the dataset for input and training of a Neural Net

--- Word2Vec.py
+++ Word2Vec.py
@@ -1,8 +1,9 @@
 import numpy as np
+from sklearn.model_selection import train_test_split        #   Probably won't use this... imported out of habit, I guess.
 
 #   I'll be using a n-gram model here to learn the word embeddings for NLP applications, or at least demonstrate the concept.
 
-corpus = "A short story is a piece of prose fiction that can be read in one sitting . Emerging from earlier oral storytelling traditions in the 17th century , the short story has grown to encompass a body of work so diverse as to defy easy characterization . At its most prototypical the short story features a small cast of named characters , and focuses on a self-contained incident with the intent of evoking a single effect or mood . In doing so , short stories make use of plot , resonance , and other dynamic components to a far greater degree than is typical of an anecdote , yet to a far lesser degree than a novel . While the short story is largely distinct from the novel , authors of both generally draw from a common pool of literary techniques . Short stories have no set length . In terms of word count there is no official demarcation between an anecdote , a short story , and a novel . Rather , the form parameters are given by the rhetorical and practical context in which a given story is produced and considered , so that what constitutes a short story may differ between genres , countries , eras , and commentators . Like the novel , the short story predominant shape reflects the demands of the available markets for publication , and the evolution of the form seems closely tied to the evolution of the publishing industry and the submission guidelines of its constituent houses . The short story has been considered both an apprenticeship form preceding more lengthy works , and a crafted form in its own right , collected together in books of similar length , price , and distribution to novels . Short story writers may define their works as part of the artistic and personal expression of the form . They may also attempt to resist categorization by genre and fixed formation"
+corpus = "A short story is a piece of prose fiction that can be read in one sitting . Emerging from earlier oral storytelling traditions in the 17th century the short story has grown to encompass a body of work so diverse as to defy easy characterization . At its most prototypical the short story features a small cast of named characters and focuses on a self-contained incident with the intent of evoking a single effect or mood . In doing so short stories make use of plot resonance and other dynamic components to a far greater degree than is typical of an anecdote yet to a far lesser degree than a novel . While the short story is largely distinct from the novel authors of both generally draw from a common pool of literary techniques . Short stories have no set length . In terms of word count there is no official demarcation between an anecdote a short story and a novel . Rather the form parameters are given by the rhetorical and practical context in which a given story is produced and considered so that what constitutes a short story may differ between genres countries eras and commentators . Like the novel the short story predominant shape reflects the demands of the available markets for publication and the evolution of the form seems closely tied to the evolution of the publishing industry and the submission guidelines of its constituent houses . The short story has been considered both an apprenticeship form preceding more lengthy works and a crafted form in its own right collected together in books of similar length price and distribution to novels . Short story writers may define their works as part of the artistic and personal expression of the form . They may also attempt to resist categorization by genre and fixed formation"
 corpus = corpus.lower() #   Convert to lowercase for consistency
 
 corpus_words = []
@@ -41,3 +42,21 @@ for sentence in sentences:      #   Sentences are cohesive structures, so I'll c
                 dataset.append([word, outputWord])
 
 print(dataset)
+print(len(dataset))
+
+def oneHotEncoding(inputSize, word):
+    inputVec = np.zeros(inputSize)
+    inputVec[intReference[word]] = 1
+    return inputVec
+
+X_train = []
+y_train = []
+
+for record in dataset:
+    X_train.append(oneHotEncoding(len(corpus_words),record[0]))
+    y_train.append(oneHotEncoding(len(corpus_words),record[1]))
+
+X_train = np.asarray(X_train)
+y_train = np.asarray(y_train)
+
+#print(X_train)
